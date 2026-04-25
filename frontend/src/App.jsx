fa6074f4d42b9665038e9e8a65b8643c100a9923
faiza-shakoor <faizashakoor24@gmail.com>
2026-04-25 13:23:19 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CreateShop from "./pages/CreateShop";
+import Profile from "./pages/Profile"; // Import your new page
 
 function App() {
   return (
-    <div>
-      <CreateShop />
-    </div>
+    <Router>
+      <div>
+        {/* This area stays the same on every page (like a Navbar) */}
+        <Routes>
+          {/* Default page (Home) can be your Profile */}
+          <Route path="/" element={<Profile />} />
+          
+          {/* Profile Page route */}
+          <Route path="/profile" element={<Profile />} />
+
+          {/* Create Shop route */}
+          <Route path="/create-shop" element={<CreateShop />} />
+          
+          {/* You can add Create Request here later when it's ready */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
